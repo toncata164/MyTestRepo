045a4b56a64ac48b2fa2e0ee013cb50119650636
Toncata <a.v.denev@gmail.com>
2021-11-29 16:20:01 +0200

    from avdenev

--- src/edu/npmg/Test.java
+++ src/edu/npmg/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello git!");
 		System.out.println("Hello from eclipse!");
+		System.out.println("Hello from avdenev");
 	}
 
 }
